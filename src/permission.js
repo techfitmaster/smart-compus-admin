4ba26cbee1e4814ace4aac968b1a05605dc774f3
Albert <1092144169@qq.com>
2019-04-29 11:12:31 +0800

    [ADD] 注册加入白名单

--- src/permission.js
+++ src/permission.js
@@ -5,11 +5,11 @@ import 'nprogress/nprogress.css'// Progress 进度条样式
 import { Message } from 'element-ui'
 import { getToken } from '@/utils/auth' // 验权
 
-const whiteList = ['/login'] // 不重定向白名单
+const whiteList = ['/login','/register'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
   NProgress.start()
   if (getToken()) {
-    if (to.path === '/login') {
+    if (to.path === ' /login') {
       next({ path: '/' })
       NProgress.done() // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
